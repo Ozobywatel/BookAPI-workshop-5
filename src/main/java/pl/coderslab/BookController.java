56ac22a95cb812ebd00f9894e35d2a41c1623385
Ozobywatel <ozobywatel@gmail.com>
2021-02-18 10:20:55 +0100

    mock book service

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -3,9 +3,13 @@ package pl.coderslab;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
@@ -13,5 +17,10 @@ public class BookController {
                 "Bruce Eckel", "Helion", "programming");
     }
 
+    @RequestMapping("/")
+    public List<Book> mockBook(){
+        MemoryBookService memoryBookService = new MemoryBookService();
+        return memoryBookService.MockBookService();
+    }
 }
 
